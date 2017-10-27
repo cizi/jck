958afe23ae449c68b6435e1ba7b1c7c4a84332cb
admin <admin>
2017-10-27 16:45:47 +0200

    - emaily neberu z pohledu ale z tabulky příspěvků

--- app/model/ArticleRepository.php
+++ app/model/ArticleRepository.php
@@ -756,7 +756,7 @@ class ArticleRepository extends BaseRepository {
 	 * @return array
 	 */
 	public function findEmailFromView() {
-		$query = "select * from v_emails_from_articles";
+		$query = "select contact_email from article where contact_email is not null";
 		$result = $this->connection->query($query)->fetchAll();
 
 		$emails = [];
